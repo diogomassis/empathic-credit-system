e985258fdd283fd656283de2fecd4eab44330f5f
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-08-19 10:34:39 -0300

    feat: Update .gitignore to include JSON files and change generate_sessions.sh to executable

--- generate_sessions.sh
+++ generate_sessions.sh
@@ -1,2 +1,1 @@
-cd k6/register-users/ && k6 run --out json=../results.json load-test.js && node process-results.js && cd ../login-users/ && k6 run --out json=../results-lo
-gin.json login-test.js && node process-logins.js && cd ../..
+cd k6/register-users/ && k6 run --out json=../results.json load-test.js && node process-results.js && cd ../login-users/ && k6 run --out json=../results-login.json login-test.js && node process-logins.js && cd ../..
